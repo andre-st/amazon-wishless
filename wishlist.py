33b14a12ba634753d4254121e13a4c8a4a685d3e
andre-st <datakadabra@gmail.com>
2019-09-04 14:18:04 +0200

    Tidy up

--- wishlist.py
+++ wishlist.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Howto:
-#	1. Run 
-#	   $ pip install scrapy
-#	   $ pip install scrapy-httpcache
-#	1. Edit settings.py
-#	2. Run scraper
-#	   $ scrapy runspider --nolog wishlists.py > output.html
-
-
 import locale
 import scrapy
 import os
