b18af55af7d1274d4acffa2fcb75922cc2d757e6
andre-st <datakadabra@gmail.com>
2020-04-22 17:26:34 +0200

    Use xsltproc if present on system

--- wishlist.sh
+++ wishlist.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-#scrapy runspider --nolog wishlist.py > output.html
-scrapy runspider wishlist.py  &&  firefox --new-tab wishlist.xml
+BROWSER="firefox"
+BROWSEROPTS=""
+
+
+scrapy runspider wishlist.py || exit 1
+
+
+if xsltproc --version > /dev/null
+then
+	echo "NOTE: Generating 'wishlist.html' from wishlist.xslt and wishlist.xml ..."
+	xsltproc wishlist.xslt wishlist.xml > wishlist.html \
+		&& $BROWSER $BROWSEROPTS wishlist.html
+else
+	echo "WARN: If displayed incorrectly, either install 'xsltproc' or visit 'about:config' and disable 'privacy.file_unique_origin'"
+	$BROWSER $BROWSEROPTS wishlist.xml
+fi
 
 
